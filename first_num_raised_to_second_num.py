b9d45f582689a53d9b12468ad11b6b34dbb02f1f
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 03:06:26 +0800

    Output given with first num raised to second num.

--- first_num_raised_to_second_num.py
+++ first_num_raised_to_second_num.py
@@ -1,2 +1,3 @@
 first_num = int(input("Enter the first number: "))
 second_num = int(input("Enter the second number: "))
+print("The first number raised to the power of the second number is:", first_num ** second_num)
