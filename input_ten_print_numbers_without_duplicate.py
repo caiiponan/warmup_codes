a822d1bcb7c06a9ffa1bc8d7651c6ddf526e3a0e
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 20:24:58 +0800

    Changing my_list to new_list inside the condition to print out the output without duplicates.

--- input_ten_print_numbers_without_duplicate.py
+++ input_ten_print_numbers_without_duplicate.py
@@ -12,5 +12,6 @@ my_list = [first, sec, third, fourth, fifth, sixth, seventh, eighth, ninth, tent
 new_list = []
 
 for i in my_list:
-    if i not in my_list:
+    if i not in new_list:
         new_list.append(i)
+print(new_list)
