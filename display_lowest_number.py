a381f2abaafd1f69b7a20507d2cb406d2e584529
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 21:31:04 +0800

    Forgot to add 'input' function.

--- display_lowest_number.py
+++ display_lowest_number.py
@@ -1,7 +1,7 @@
 my_list: []
 
 def ask():
-    num_input = ("Enter a number: ")
+    num_input = input("Enter a number: ")
     if num_input.isdigit == True:
         if num_input not in my_list:
             my_list.append(num_input)
