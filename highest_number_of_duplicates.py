b929b5a818f52bc52a98c27b01d6b00ab1cf174b
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 22:54:33 +0800

    Checking if the input given is a digit so it can be added into the list and dictionary.

--- highest_number_of_duplicates.py
+++ highest_number_of_duplicates.py
@@ -2,4 +2,7 @@ my_list = []
 count_dict = {}
 def ask():
     num_input = input("Enter a number: ")
-    
+    if num_input.isdigit():
+        num_input = int(num_input)
+        my_list.append(num_input)
+        
