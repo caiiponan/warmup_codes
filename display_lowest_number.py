3d88729b5110b35f10988eb68ece4645a6594633
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 21:51:29 +0800

    Patched the digit checker into only integers.

--- display_lowest_number.py
+++ display_lowest_number.py
@@ -1,12 +1,11 @@
 my_list = []
 
 def ask():
-    num_input = input("Enter a number: ")
-    if num_input.isdigit() == True:
-        if num_input not in my_list:
-            my_list.append(num_input)
-            print(f"The smalles number so far is: {min(my_list)}")
-            ask()
+    num_input = int(input("Enter a number: "))
+    if type(num_input) == int:
+        my_list.append(num_input)
+        print(f"The smallest number so far is: {min(my_list)}")
+        ask()
     else:
         exit
 ask()
