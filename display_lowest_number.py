d765460a40f9a099decdd8cb6bf7025744a89e19
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 21:44:08 +0800

    Fixed typos and syntax errors.

--- display_lowest_number.py
+++ display_lowest_number.py
@@ -1,11 +1,12 @@
-my_list: []
+my_list = []
 
 def ask():
     num_input = input("Enter a number: ")
-    if num_input.isdigit == True:
+    if num_input.isdigit() == True:
         if num_input not in my_list:
             my_list.append(num_input)
-            print(min(my_list))
+            print(f"The smalles number so far is: {min(my_list)}")
+            ask()
     else:
         exit
 ask()
