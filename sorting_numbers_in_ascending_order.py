26f284a4719b2b913ed18f9fe29d737599dc25e0
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 22:06:59 +0800

    Checking if the input is already inside the list to add it.

--- sorting_numbers_in_ascending_order.py
+++ sorting_numbers_in_ascending_order.py
@@ -4,3 +4,5 @@ def ask():
     num_input = (input("Enter a number: "))
     if num_input.isdigit() == True:
         num_input = int(num_input)
+        if num_input not in my_list:
+            my_list.append(num_input)
