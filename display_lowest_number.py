defd9cf6ee04d8f71a4d8cef48d72265b9e27382
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 21:29:02 +0800

    Adding every input inside the list to keep it updated.

--- display_lowest_number.py
+++ display_lowest_number.py
@@ -3,4 +3,5 @@ my_list: []
 def ask():
     num_input = ("Enter a number: ")
     if num_input.isdigit == True:
-        
+        if num_input not in my_list:
+            my_list.append(num_input)
