cec22c9dda2410111f2bcc7a4da6aaf3d287808e
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 23:18:21 +0800

    Appending the input into the list and then sorting the input of the user.

--- sorting_numbers_in_descending_order.py
+++ sorting_numbers_in_descending_order.py
@@ -4,4 +4,7 @@ def ask():
     num_input = (input("Enter a number: "))
     if num_input.isdigit() == True:
         num_input = int(num_input)
-        
+        if num_input not in my_list:
+            my_list.append(num_input)
+            my_list.sort(reverse=True)
+            
