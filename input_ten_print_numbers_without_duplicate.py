0077729e702c17bff1440e916b7c2bad31236b02
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 20:17:47 +0800

    Put everything in a list.

---
+++ input_ten_print_numbers_without_duplicate.py
@@ -0,0 +1,11 @@
+first = int(input("First number: "))
+sec = int(input("Second number: "))
+third = int(input("Third number: "))
+fourth = int(input("Fourth number: "))
+fifth = int(input("Fifth number: "))
+sixth = int(input("Sixth number: "))
+seventh = int(input("Seventh number: "))
+eighth = int(input("Eighth number: "))
+ninth = int(input("Ninth number: "))
+tenth = int(input("Tenth number: "))
+my_list = [first, sec, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth]
