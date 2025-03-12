a8c32729ffff76421089027cbdfb5f5d2a9a6b84
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-12 21:21:22 +0800

    Checking if the input is not in the list to say that the given number is unique.

--- print_unique_when_number_not_have_duplicate_else_print_duplicate.py
+++ print_unique_when_number_not_have_duplicate_else_print_duplicate.py
@@ -3,4 +3,7 @@ my_list = []
 def ask():
     num_input =(input("Enter a number: "))
     if num_input.isdigit() == True:
-        
+        if num_input not in my_list:
+            print(f"{num_input} - UNIQUE")
+            my_list.append(num_input)
+            ask()
